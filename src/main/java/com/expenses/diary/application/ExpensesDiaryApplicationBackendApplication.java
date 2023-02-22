84e18076a1155339d718822c821d4a6f8db5d188
codingwithadi <adityarshinde1999@gmail.com>
2023-02-22 17:47:20 +0530

    expenses-diary-application

---
+++ src/main/java/com/expenses/diary/application/ExpensesDiaryApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.expenses.diary.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesDiaryApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesDiaryApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expenses/diary/application/ExpensesDiaryApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenses.diary.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesDiaryApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
