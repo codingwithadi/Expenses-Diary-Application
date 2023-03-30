ca3f5ca4fd729b0777cf941331372a31e3300e32
codingwithadi <adityarshinde1999@gmail.com>
2023-03-30 14:04:51 +0530

    add swagger for api docs

--- src/main/java/com/expenses/diary/application/ExpensesDiaryApplicationBackendApplication.java
+++ src/main/java/com/expenses/diary/application/ExpensesDiaryApplicationBackendApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Expenses Diary Application APIs", version = "1.0.0"))
 public class ExpensesDiaryApplicationBackendApplication {
 
 	public static void main(String[] args) {
